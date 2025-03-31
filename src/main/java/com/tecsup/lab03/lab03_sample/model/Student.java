41acd6401423d5260240831d1b70d90d1668380f
Alex Marín Méndez <amarinm@tecsup.edu.pe>
2025-03-31 16:09:43 -0500

    add Student model

---
+++ src/main/java/com/tecsup/lab03/lab03_sample/model/Student.java
@@ -0,0 +1,15 @@
+package com.tecsup.lab03.lab03_sample.model;
+
+public class Student {
+	private String name;
+	private int age;
+    
+	public Student(String name, int age) {
+		this.name = name;
+		this.age = age;
+	}
+
+	public String getName() { return name; }
+    public int getAge() { return age; }
+
+}
